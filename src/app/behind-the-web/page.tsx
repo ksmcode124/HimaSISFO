4f973acb902a2736c51b7f78a01286fe6c2fd691
dndraamy <dndramylza12@gmail.com>
2025-10-27 02:40:14 +0700

    feat: add sosmed component & dependency

--- src/app/behind-the-web/page.tsx
+++ src/app/behind-the-web/page.tsx
@@ -1,3 +1,24 @@
-export default function Page() {
-  return <h1>tes aja</h1>;
+import { SocialMediaLink } from "./components/SocialMediaLink";
+
+const socialMediaList = [
+  { platform: "instagram", href: "https://www.instagram.com/the.code124" },
+  { platform: "linkedin", href: "https://www.linkedin.com/..." },
+  { platform: "whatsapp", href: "https://wa.me/..." },
+  { platform: "email", href: "mailto:test@example.com" },
+] as const;
+
+export default function BehindTheWeb() {
+  return (
+    <>
+      <div>
+        {socialMediaList.map((social) => (
+          <SocialMediaLink
+            key={social.platform}
+            platform={social.platform}
+            href={social.href}
+          />
+        ))}
+      </div>
+    </>
+  );
 }
