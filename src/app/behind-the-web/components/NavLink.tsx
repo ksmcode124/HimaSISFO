8039e7ddd7cb6f57d6819a84e859559875176054
IvandryPage <ivandry.89@gmail.com>
2025-10-29 22:20:23 +0700

    fix(behind-the-web): update NavLink border

--- src/app/behind-the-web/components/NavLink.tsx
+++ src/app/behind-the-web/components/NavLink.tsx
@@ -1,18 +1,23 @@
-import Link from "next/link"
+import Link from "next/link";
 import React from "react";
 
 interface NavProps {
-  href: string,
-  label?: string,
-  className?: string,
-  children?: React.ReactNode
+  href: string;
+  children?: React.ReactNode;
 }
 
-export function NavLink({href, label, className, children} : NavProps) {
+export function NavLink({ href, children }: NavProps) {
   return (
     <>
-      <Link href={href} className="rounded-2xl px-3 py-1 bg-[#3A484F]/50 shadow-[0_4px_4px_rgba(0,0,0,0.25)] transition-shadow hover:shadow-[inset_5px_5px_4px_rgba(0,0,0,0.4),0_4px_4px_rgba(0,0,0,0.25)]">
-        {children ?? label}
+      <Link
+        href={href}
+        className="relative rounded-xl lg:rounded-2xl px-2 py-1.5 lg:px-3 lg:py-1.5 text-center text-sm lg:text-base bg-[#3A484F]/50
+          shadow-[0_2px_3px_rgba(0,0,0,0.25)] lg:shadow-[0_4px_4px_rgba(0,0,0,0.25)] transition-all active:scale-[0.97]
+          hover:shadow-[inset_5px_5px_4px_rgba(0,0,0,0.4),0_4px_4px_rgba(0,0,0,0.25)] ßtouch-manipulation"
+      >
+        <span className="absolute inset-0 rounded-xl lg:rounded-2xl ring-1 ring-white/30 [mask-[linear-gradient(to-bottom_right,white,transparent)]] pointer-events-none"></span>
+
+        {children}
       </Link>
     </>
   );
