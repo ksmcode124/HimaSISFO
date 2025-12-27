e4bcffee901808b4607099a9474d93820969b0ce
Indra-Haha <124240180@student.upnyk.ac.id>
2025-12-28 00:43:41 +0700

    style(UI): border tampilan kasar

--- src/features/kegiatan/calendar/CalendarContent.tsx
+++ src/features/kegiatan/calendar/CalendarContent.tsx
@@ -1,13 +1,16 @@
 import DynamicCalendar from "../Calendar"
+
 export function CalendarContent() {
-    return (
-        <div className="relative border-2 border-red">
-            <h1 className="text-xl text-center">Kalender</h1>
-            <DynamicCalendar />
-            <div className="p-5 h-fit">
-                <h2>keterangan</h2>
-                <p>Ini adalah keterangan untuk kalender.</p>
-            </div>
-        </div>
-    )
+  return (
+    <div className="relative flex flex-col gap-5 border-2 border-yellow-500">
+      <h1 className="py-4 md:py-10 text-xl text-center border-amber-200 border-2">
+        Kalender
+      </h1>
+      <DynamicCalendar className="border-red-500 border-2"/>
+      <div className="p-5 border-2 border-accent">
+        <h2>Keterangan</h2>
+        <p>Ini adalah keterangan untuk kalender.</p>
+      </div>
+    </div>
+  )
 }
