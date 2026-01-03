532d0c27e79ee6bfddcb0183164197774490c062
Indra-Haha <124240180@student.upnyk.ac.id>
2026-01-03 08:49:26 +0700

    feat(UI) : penambahan detail event layout

---
+++ src/features/kegiatan/sections/EventDetailSection.tsx
@@ -0,0 +1,8 @@
+import { EventDetailContent } from "../components/layer/ContentLayer"
+export function EventDetailSection() {
+    return (
+        <section className="relative">
+            <EventDetailContent />
+        </section>
+    )
+}
