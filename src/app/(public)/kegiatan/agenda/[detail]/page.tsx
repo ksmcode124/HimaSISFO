835605d2330735a5af1ec881c682ca1402d72c84
Indra-Haha <124240180@student.upnyk.ac.id>
2026-01-04 19:40:53 +0700

    chore(structure)

--- src/app/(public)/kegiatan/agenda/[detail]/page.tsx
+++ src/app/(public)/kegiatan/agenda/[detail]/page.tsx
@@ -1,9 +1,9 @@
 import { EventDetailSection } from "@/features/kegiatan/sections/EventDetailSection"
-export default function Page() {
+export default function Page({params} : {params: { detail: string }}) {
     return (
         <div>
             <div className="relative my-[150px] mx-20">
-                <EventDetailSection />
+                <EventDetailSection searchKeyword={params.detail} />
             </div>
         </div>
     )
---
+++ src/app/(public)/kegiatan/agenda/page.tsx
@@ -0,0 +1,9 @@
+import { EventListSection } from "@/features/kegiatan/sections/EventListSection";
+
+export default function Page() {
+  return (
+    <div>
+      <EventListSection  />
+    </div>
+  );
+}
