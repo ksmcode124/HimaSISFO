4d9b64afa9bc5186a74186bdc05d67f5a44c00f1
Indra-Haha <124240180@student.upnyk.ac.id>
2026-01-04 14:44:03 +0700

    perf(data) : creating dynamic content

--- src/features/kegiatan/sections/EventCardSection.tsx
+++ src/features/kegiatan/sections/EventCardSection.tsx
@@ -5,14 +5,17 @@ import {
  } from '@/components/layout/Layer'
 import { EventCardContent } from '../components/layer/ContentLayer'
 import { EventCardDecoration } from '../components/layer/DecorationLayer'
-export function EventCardSection() {
+import { getEvents } from '../services/eventService';
+ 
+export async function EventCardSection() {
+    const events = await getEvents();
     return (
         <section className="relative px-0 m-0">
             <DecorationLayer className="h-[231px] md:h-[617px] mt-35 md:mt-60">
                 <EventCardDecoration />
             </DecorationLayer>
             <ContentLayer className="px-2 md:px-20 xl:px-40 pb-30">
-                <EventCardContent />
+                <EventCardContent events={events} />
             </ContentLayer>
             <OverlayLayer>
                 <div></div>
