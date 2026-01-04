daa303a42b4655b255525d1ad795c4488fd8b114
Indra-Haha <124240180@student.upnyk.ac.id>
2026-01-04 19:30:22 +0700

    feat(logic) : penambahan index untuk filter

---
+++ src/features/kegiatan/utils/EventIndexer.ts
@@ -0,0 +1,35 @@
+// Fungsi ini hanya dijalankan SEKALI untuk memproses seluruh data
+export function createEventIndex(events: any[]) {
+  const eventMap: Record<string, any[]> = {};
+
+  events.forEach((event) => {
+    const d = new Date(event.date);
+    if (isNaN(d.getTime())) return;
+
+    const monthName = d.toLocaleString('id-ID', { month: 'long' }); 
+    const year = d.getFullYear();
+    const key = `${monthName}-${year}`;
+
+    if (!eventMap[key]) {
+      eventMap[key] = [];
+    }
+    eventMap[key].push(event);
+  });
+  return eventMap;
+}
+
+export function createEventIndexByNameDate(events: any[]) {
+  const eventMap: Record<string, any[]> = {};
+
+  events.forEach((event) => {
+    const { id } = event;
+    const key = `${id}`;
+
+    if (!eventMap[key]) {
+      eventMap[key] = [];
+    }
+    eventMap[key].push(event);
+  });
+  return eventMap;
+}
+
