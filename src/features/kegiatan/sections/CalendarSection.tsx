6f6c568a1c033159d6acf8d0a2e4b8d9d23a3dfa
Indra-Haha <124240180@student.upnyk.ac.id>
2026-01-04 14:43:31 +0700

    perf(data) : creating dynamic content

--- src/features/kegiatan/sections/CalendarSection.tsx
+++ src/features/kegiatan/sections/CalendarSection.tsx
@@ -7,15 +7,16 @@ import {
 import { CalendarOverlay } from "../components/layer/OverlayLayer"
 import { CalendarContent } from "../components/layer/ContentLayer"
 import { CalendarDecoration } from "../components/layer/DecorationLayer"
-
-export function CalendarSection() {
+import { getEvents } from "../services/eventService";
+export async function CalendarSection() {
+  const events = await getEvents();
   return (
     <section className="relative p-0 m-0 items-center justify-center">
       <DecorationLayer className="px-0">
         <CalendarDecoration />
       </DecorationLayer>
       <ContentLayer className="md:px-30 xl:px-60 pb-30">
-        <CalendarContent />
+        <CalendarContent events={events} />
       </ContentLayer>
       <OverlayLayer className="px-5 justify-center">
         <CalendarOverlay />
