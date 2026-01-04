f6f05fcb6694d1014f2cd69c496c728a7dfb5e5d
Indra-Haha <124240180@student.upnyk.ac.id>
2026-01-04 19:39:42 +0700

    perf(data): parsing data

--- src/features/kegiatan/sections/EventListSection.tsx
+++ src/features/kegiatan/sections/EventListSection.tsx
@@ -2,14 +2,16 @@ import {
     DecorationLayer, 
     ContentLayer, 
     OverlayLayer, 
- } from '@/components/layout/Layer'
-import { EVENTS } from '../data/events';    
+ } from '@/components/layout/Layer'   
 import { EventListContent } from '../components/layer/ContentLayer';
-export function EventListSection() {
+import { getEvents } from '../services/eventService';
+
+export async function EventListSection() {
+  const events = await getEvents();
   return (
     <section className="relative px-0 m-0">
-      <ContentLayer>
-        <EventListContent events={EVENTS as any} />
+      <ContentLayer className="mx-10">
+        <EventListContent events={events} />
       </ContentLayer>
     </section>
   );
