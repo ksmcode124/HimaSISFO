60666c49ab83fe03e13c79227d0ad6e2afe77895
Indra-Haha <124240180@student.upnyk.ac.id>
2026-01-05 00:38:11 +0700

    build(setup) : refactor dari beberapa file

--- src/features/kegiatan/types.ts
+++ src/features/kegiatan/types.ts
@@ -21,3 +21,24 @@ export interface EventDetailContentProps {
 export interface WithVariantEventCardProps extends EventCardProps {
   variant?: "detail" | "onGoing" | "notGoing";
 }
+
+export interface EventWithVariantProps extends EventCardProps {
+  date: Date;
+}
+
+export interface SortedSingleEventsProps {
+  pastNotGoing?: EventWithVariantProps;
+  nextOnGoing?: EventWithVariantProps;
+  futureNotGoing?: EventWithVariantProps;
+}
+
+export interface FindEventByIdProps {
+  id: number;
+  indexedEvents: Record<string, any[]>;
+}
+
+export interface FindEventByMonthYearProps {
+  month: string;
+  year: number;
+  indexedEvents: Record<string, any[]>;
+}
