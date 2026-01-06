d33fd26fd9c5d064c0016f55b280cfd6cc603bc5
Indra-Haha <124240180@student.upnyk.ac.id>
2026-01-06 09:23:48 +0700

    feat(data) : add filter parameter

--- src/features/kegiatan/sections/EventDetailSection.tsx
+++ src/features/kegiatan/sections/EventDetailSection.tsx
@@ -2,12 +2,12 @@
 import { EventDetailContent } from "../components/layer/ContentLayer"
 import { getEvents } from "../services/eventService";
 import { ContentLayer } from "@/components/layout/Layer";
-export async function EventDetailSection({searchKeyword} : {searchKeyword: string}) {
+export async function EventDetailSection({id} : {id: string}) {
     const events = await getEvents();
     return (
         <section className="relative p-0 m-0 top-0">
             <ContentLayer className="mx-10 md:mx-30">
-                <EventDetailContent events={events} search={searchKeyword} />
+                <EventDetailContent events={events} search={id} />
             </ContentLayer>
         </section>
     )
