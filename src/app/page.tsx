cf412bd89f1a0b7dc36b18b3afa10a897119da3c
Indra-Haha <124240180@student.upnyk.ac.id>
2026-01-09 18:41:34 +0700

    style(UI) : remove tes task

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,3 @@
-// app/kegiatan/page.tsx
-import { FilterComp } from "@/features/kegiatan/components/FilterComp";
-
-export default async function Page({ 
-  searchParams 
-}: { 
-  searchParams: Promise<{ bulan?: string }> 
-}) {
-  // Ambil nilai 'bulan' dari URL GET param
-  const { bulan } = await searchParams;
-  const currentBulan = bulan || "Januari"; // default
-
-  console.log("Server sedang render bulan:", currentBulan);
-
-  // CONTOH: Fetch data di server berdasarkan bulan
-  // const data = await fetch(`https://api.anda.com/data?bulan=${currentBulan}`).then(res => res.json());
-
-  return (
-    <div className="p-10">
-      <h1 className="text-xl font-bold mb-4">Halaman Kegiatan: {currentBulan}</h1>
-      
-      {/* Filter ini akan mengubah URL saat diklik */}
-      <FilterComp />
-
-      <div className="mt-8">
-        {/* Render data hasil filter di sini */}
-        <p>Data untuk bulan {currentBulan} akan muncul di sini...</p>
-      </div>
-    </div>
-  );
+export function page (){
+  return <h1>Hello World</h1>
 }
