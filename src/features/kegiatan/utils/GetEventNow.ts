83c1058c1f3e6e3275a89eb8704f606d33798e24
Indra-Haha <124240180@student.upnyk.ac.id>
2026-01-09 18:37:05 +0700

    test(nama) : penyesuaian data nama event

--- src/features/kegiatan/utils/GetEventNow.ts
+++ src/features/kegiatan/utils/GetEventNow.ts
@@ -9,16 +9,16 @@ export function sortEvents(events: EventWithVariantProps[]): SortedSingleEventsP
   const now = new Date();
 
   const pastNotGoing = events
-    .filter(e => e.date < now)
-    .sort((a, b) => b.date.getTime() - a.date.getTime())[0];
+    .filter(e => e.start < now)
+    .sort((a, b) => b.start.getTime() - a.start.getTime())[0];
 
   const nextOnGoing = events
-    .filter(e => e.date >= now)
-    .sort((a, b) => a.date.getTime() - b.date.getTime())[0];
+    .filter(e => e.start >= now)
+    .sort((a, b) => a.start.getTime() - b.start.getTime())[0];
 
   const futureNotGoing = events
-    .filter(e => e.date > nextOnGoing.date)
-    .sort((a, b) => a.date.getTime() - b.date.getTime())[0];
+    .filter(e => e.start > nextOnGoing.start)
+    .sort((a, b) => a.start.getTime() - b.start.getTime())[0];
 
   return { pastNotGoing, nextOnGoing, futureNotGoing };
 }
