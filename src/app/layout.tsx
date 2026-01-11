9af932f6c87187bc4ba2da403d4a3ff3ce6bd3af
Indra-Haha <124240180@student.upnyk.ac.id>
2026-01-11 14:28:30 +0700

    chore: refactor function menjadi 1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,28 @@
-// src/app/layout.tsx
+import type { Metadata } from "next";
 import "./globals.css";
+import { Poppins } from 'next/font/google';
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+  variable: '--font-primary'
+});
+
+export const metadata: Metadata = {
+  title: "Himpunan Mahasiswa Sistem Informasi UPNVYK",
+  description: "Website Portfolio Himpunan Mahasiswa Sistem Informasi UPN Veteran Yogyakarta",
+};
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
-    <html lang="id" className="scroll-smooth" data-scroll-behavior="smooth">
-      <body className="antialiased">
+    <html lang="id">
+      <body
+        className={`${poppins.variable} antialiased`}
+      >
         {children}
       </body>
     </html>
--- src/features/kegiatan/sections/EventListSection.tsx
+++ src/features/kegiatan/sections/EventListSection.tsx
@@ -11,11 +11,7 @@ import { findEventByMonthYear } from '../utils/GetEventNow';
 import { EventCard } from '../components/EventCard';
 import { FilterComp } from '../components/FilterComp';
 
-function EventListContent({ events, filter }: { events: EventCardProps[], filter?: string }) {
-  const tahunIni = new Date().getFullYear();
-  const bulan = filter || formatMonthName(new Date().getMonth());
-  const HelperEvent = createEventIndex(events);
-  const FindEvent = findEventByMonthYear({ month: bulan, year: tahunIni, indexedEvents: HelperEvent });
+function EventListContent({ events }: { events: EventCardProps[]}) {
   return (
     <div className="relative justify-center items-center">
       <h2 className="text-base md:text-xl w-full h-fit py-3 md:py-5 border-b-2 border-black">
@@ -27,10 +23,10 @@ function EventListContent({ events, filter }: { events: EventCardProps[], filter
         </h1>
         <div className="text-base md:text-4xl flex flex-row justify-between items-center py-3 md:py-5">
           <FilterComp className="text-xl" />
-          <span className="text-[14px] md:text-xl">{FindEvent.length} Acara ditemukan</span>
+          <span className="text-[14px] md:text-xl">{events.length} Acara ditemukan</span>
         </div>
         <div className="grid grid-cols-2 md:grid-cols-3 gap-5 md:gap-10 mt-5 md:mt-10">
-          {FindEvent.map((event) => (
+          {events.map((event) => (
             <EventCard
               key={event.id}
               id={event.id}
@@ -59,11 +55,16 @@ function EventListContent({ events, filter }: { events: EventCardProps[], filter
   );
 }
 export async function EventListSection({ filter }: { filter?: string }) {
+  const tahunIni = new Date().getFullYear();
+  const bulan = filter || formatMonthName(new Date().getMonth());
   const events = await getEvents();
+  const HelperEvent = createEventIndex(events);
+  const FindEvent = findEventByMonthYear({ month: bulan, year: tahunIni, indexedEvents: HelperEvent });
+  
   return (
     <section className="relative p-0 m-0 top-0">
       <ContentLayer className="mx-10 md:mx-30">
-        <EventListContent events={events} filter={filter} />
+        <EventListContent events={FindEvent}/>
       </ContentLayer>
     </section>
   );
