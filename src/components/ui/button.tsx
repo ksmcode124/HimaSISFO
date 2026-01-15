8bbad3441fa1fff1ae6d1ffcd4bf61c3f6427a65
Indra-Haha <124240180@student.upnyk.ac.id>
2026-01-15 20:45:19 +0700

    style: size lg & variant default

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -26,7 +26,7 @@ const buttonVariants = cva(
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
         sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
-        lg: "rounded-full p-2 md:p-8",
+        lg: "rounded-full p-2 md:p-4",
         icon: "size-9",
         "icon-sm": "size-8",
         "icon-lg": "size-10",
