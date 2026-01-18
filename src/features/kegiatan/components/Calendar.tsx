09182617854b722ee0302eec3209bdf3937f94be
Indra-Haha <124240180@student.upnyk.ac.id>
2026-01-18 12:00:59 +0700

    perf : apply style with data dynamic

--- src/features/kegiatan/components/Calendar.tsx
+++ src/features/kegiatan/components/Calendar.tsx
@@ -7,38 +7,46 @@ import { toDateKey, getMonthDays } from "../utils/Calculate";
 import { formatMonthName, formatYear } from "../utils/FormatDate"
 import { WEEK_DAYS } from "../data/constant";
 import { DynamicCalendarProps } from "../types";
+import { Modal } from "./PopUp";
+import { parseEventDate } from "../utils/ParseDate";
+import { sortEventsByDay } from "../utils/SortEvent";
+import { getEventSegment } from "../utils/EventSegment";
+import { getSegmentClass } from "../utils/EventClass";
+import { FilterComp } from "./FilterComp";
 
 export function DynamicCalendar({
   className,
   events,
 }: DynamicCalendarProps) {
   const today = new Date();
-  const [year, setYear] = useState(today.getFullYear());
-  const [month, setMonth] = useState(today.getMonth());
+  const [year, setYear] = useState<number>(today.getFullYear());
+  const [month, setMonth] = useState<number>(today.getMonth());
+  const [open, setOpen] = useState(false);
+  const [selectedEvent, setSelectedEvent] = useState<null | {
+    title: string;
+    start: Date;
+    end: Date;
+  }>(null);
+
 
   const daysGrid = useMemo(() => getMonthDays(year, month), [year, month]);
   const eventsByDate = useMemo(() => {
     const map: Record<string, typeof events> = {};
 
     (events || []).forEach(event => {
-      const start = event.start instanceof Date
-        ? new Date(event.start)
-        : new Date(event.start);
-
+      const start = parseEventDate(event.start);
       const end = event.end
-        ? event.end instanceof Date
-          ? new Date(event.end)
-          : new Date(event.end)
+        ? parseEventDate(event.end)
         : start;
 
-      start.setHours(0, 0, 0, 0);
-      end.setHours(0, 0, 0, 0);
+      // clone khusus untuk perhitungan tanggal
+      const startDay = new Date(start);
+      const endDay = new Date(end);
 
-      for (
-        let d = new Date(start);
-        d <= end;
-        d.setDate(d.getDate() + 1)
-      ) {
+      startDay.setHours(0, 0, 0, 0);
+      endDay.setHours(0, 0, 0, 0);
+
+      for (let d = new Date(startDay); d <= endDay; d.setDate(d.getDate() + 1)) {
         if (
           d.getFullYear() !== year ||
           d.getMonth() !== month
@@ -72,24 +80,21 @@ export function DynamicCalendar({
       setMonth(0);
     } else setMonth(m => m + 1);
   };
-
-  const goToday = () => {
-    setYear(today.getFullYear());
-    setMonth(today.getMonth());
-  };
+  // console.log(month, year)
   return (
     <div className={clsx(className, "w-full py-4 px-2 md:px-4 bg-white rounded-lg shadow")}>
       <div className="flex flex-row w-full items-center justify-between my-5">
         <h2 className="text-[20px] md:text-xl font-medium md:font-bold text-[var(--color-dark-blue)]">
           {String(formatMonthName(month))}, {String(formatYear(year))}
         </h2>
-        <div className="flex border-3 border-[var(--color-dark-blue)] rounded overflow-hidden">
+        <div className="relative flex flex-row border-3 border-[var(--color-dark-blue)] rounded">
           <button onClick={goPrevMonth} className="p-1 border-r-3 border-[var(--color-dark-blue)]">
             <ChevronLeft />
           </button>
-          <button onClick={goToday} className="w-[100px] md:w-[116px] py-2 font-bold text-sm uppercase">
-            {String(formatMonthName(month)).slice(0, 3)} {String(formatYear(year))}
-          </button>
+          <FilterComp className="w-[100px] md:w-[140px] py-1 md:py-2 font-bold uppercase" type="calendar" selected={`${String(formatMonthName(month)).slice(0, 3)} ${Number(formatYear(year))}`} onChange={(month, year) => {
+            setMonth(month-1);
+            setYear(year as number);
+          }}/>
           <button onClick={goNextMonth} className="p-1 border-l-3 border-[var(--color-dark-blue)]">
             <ChevronRight />
           </button>
@@ -114,11 +119,15 @@ export function DynamicCalendar({
             >
               {daysGrid
                 .slice(weekIdx * 7, weekIdx * 7 + 7)
-                .map(({ day, isCurrentMonth, dateObj }, dayIdx) => {
+                .map(({ day, isCurrentMonth, dateObj }, idx) => {
                   const key = toDateKey(dateObj);
                   const dayEvents = eventsByDate[key];
+                  const sortedEvents = sortEventsByDay(
+                    dayEvents ?? [],
+                    dateObj
+                  );
+                  // console.log(idx, day, sortedEvents)
                   const isToday = key === toDateKey(today);
-                  const posisi = dayIdx + 1;
                   return (
                     <td
                       key={key}
@@ -127,13 +136,13 @@ export function DynamicCalendar({
                         isCurrentMonth ? "bg-[#99B6D9]" : "bg-[#B3C9E4]"
                       )}
                     >
-                      <div className="flex flex-col items-start aspect-[12/14] md:aspect-[12/13]">
+                      <div className="flex flex-col items-start aspect-[12/14] md:aspect-[1/1]">
                         <div className="h-[45%] md:h-[35%] w-full flex justify-start items-start pl-1 md:pl-2 pt-1 md:pt-2">
                           <div
                             className={clsx(
                               "flex items-center justify-center font-medium leading-none text-[var(--color-dark-blue)]",
                               isCurrentMonth ?
-                               "opacity-100" : "opacity-50",
+                                "opacity-100" : "opacity-50",
                               isToday
                                 ? "bg-[#3978FF] rounded-full w-5 h-5 md:w-8 md:h-8 text-white"
                                 : ""
@@ -145,42 +154,63 @@ export function DynamicCalendar({
 
 
                         <div className="flex flex-col w-full gap-[2px] md:gap-2 overflow-visible">
-                          {dayEvents?.map((ev) => (
-                            <div
-                              key={`${ev.id}-${ev.start}`}
-                              className={clsx(
-                                "text-[10px] md:text-sm font-bold h-[15px] md:h-[30px] w-auto truncate text-white items-center flex ",
-                                ev.start.getDate().toString().trim() === ev.end.getDate().toString().trim() ? "mx-1 rounded-[5px] px-2" :
-                                  ev.start.getDate().toString().trim() === day.toString().trim() ? "ml-1 rounded-l-[5px] px-2" :
-                                    ev.end.getDate().toString().trim() === day.toString().trim() ? "mr-1 rounded-r-[5px]" :
-                                      day > ev.start.getDate() && day < ev.end.getDate() && posisi % 7 === 0 ? "-ml-1" :
-                                        day > ev.start.getDate() && day < ev.end.getDate() && posisi % 7 === 1 ? "-mr-1" : "-mx-1",
-                                ev.type === "Hima" && "bg-gradient-to-b from-[#1B3C53] to-[#456882]",
-                                ev.type === "Beasiswa" && "bg-gradient-to-b from-[#7F1D1D] to-[#DC2626]",
-                                ev.type === "Lomba" && "bg-gradient-to-b from-[#CA8A04] to-[#EAB308]"
-                              )}
-                            >
-                              {ev.start.getDate().toString().trim() === ev.end.getDate().toString().trim() ?
-                                <>{ev.title}</> :
-                                ev.start.getDate().toString().trim() === day.toString().trim() ?
-                                  <>{ev.title}</> :
-                                  ev.end.getDate().toString().trim() === day.toString().trim() ?
-                                    <></> :
-                                    day > ev.start.getDate() && day < ev.end.getDate() ?
-                                      <></> : null}
-                            </div>
-                          ))}
+                          {sortedEvents?.map((ev) => {
+                            const currentDay = new Date(year, month, day);
+                            const segment = getEventSegment(
+                              ev.start,
+                              ev.end,
+                              currentDay,
+                              idx
+                            );
+                          
+                            if (!segment) return null; return (
+                              <div
+                                key={`${ev.id}-${currentDay.toDateString()}`}
+                                className={clsx(
+                                  "text-[10px] md:text-sm font-bold h-[15px] md:h-[30px] truncate text-white flex items-center cursor-pointer",
+                                  getSegmentClass(segment),
+                                  ev.type === "Hima" && "bg-gradient-to-b from-[#1B3C53] to-[#456882]",
+                                  ev.type === "Beasiswa" && "bg-gradient-to-b from-[#7F1D1D] to-[#DC2626]",
+                                  ev.type === "Lomba" && "bg-gradient-to-b from-[#CA8A04] to-[#EAB308]"
+                                )}
+                                onClick={(e) => {
+                                  e.stopPropagation();
+                                  setSelectedEvent(ev);
+                                  setOpen(true);
+                                }}
+                              >
+                                {(segment === "single" || segment === "head") && (
+                                  <>
+                                    {ev.title}
+                                  </>
+                                )}
+                              </div>
+                            );
+                          })}
                         </div>
 
                       </div>
                     </td>
                   );
                 })}
+
             </tr>
           ))}
+          {selectedEvent && (
+            <Modal
+              open={open}
+              onClose={() => setOpen(false)}
+              event={{
+                title: selectedEvent.title,
+                start: selectedEvent.start,
+                end: selectedEvent.end,
+              }}
+              type="calendar"
+            />
+          )}
         </tbody>
       </table>
-    </div>
+    </div >
   );
 
 }
