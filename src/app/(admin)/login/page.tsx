f2285fb3c7effa20587f577a127c8541283a6b66
IvandryPage <ivandry.89@gmail.com>
2026-01-25 01:47:46 +0700

    style: update styling on page admin

--- src/app/(admin)/login/page.tsx
+++ src/app/(admin)/login/page.tsx
@@ -9,6 +9,7 @@ import {
   FieldSet,
 } from '@/components/ui/field';
 import { Input } from '@/components/ui/input';
+import TextSlideshow from '@/features/admin/components/TextSlideshow';
 import { useLogin } from '@/features/admin/hooks/useLogin';
 import Image from 'next/image';
 
@@ -25,58 +26,65 @@ export default function LoginPage() {
 
   return (
     <>
-      <header className="flex h-[10vh] w-screen items-center gap-5 bg-[#01161E] px-5 text-white">
+      <header className="flex h-[10vh] w-screen items-center gap-5 px-5">
         <div className="relative aspect-square h-15">
           <Image priority fill src='/assets/shared/logos/logo-himasisfo.webp' alt={''} />
         </div>
-        <p className="text-4xl font-bold italic">DASHBOARD</p>
+        <p className="text-2xl font-bold">DASHBOARD</p>
       </header>
-      <section className="grid h-[90vh] w-full items-center justify-items-center bg-white">
-        <form
-          className="h-[50%] w-[35%] rounded-2xl bg-[#102F41] p-10 text-white"
-          onSubmit={handleSubmit}
-        >
-          <FieldSet className="flex h-full flex-col items-center justify-around">
-            <FieldLegend className="text-center">
-              <span className="text-4xl font-bold italic">LOGIN</span>
-              {error && (
-                <div className="rounded-lg border border-red-200 bg-red-50 px-4 py-3 text-red-700 dark:border-red-800 dark:bg-red-900/20 dark:text-red-400">
-                  {error}
-                </div>
-              )}
-            </FieldLegend>
-            <FieldGroup>
-              <Field>
-                <FieldLabel htmlFor="username" className="text-2xl font-bold">
-                  Email
-                </FieldLabel>
-                <Input
-                  className="bg-white text-black"
-                  id="username"
-                  type="text"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-              </Field>
-              <Field>
-                <FieldLabel htmlFor="password" className="text-2xl font-bold">
-                  Password
-                </FieldLabel>
-                <Input
-                  className="bg-white text-black"
-                  id="password"
-                  type="password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </Field>
-            </FieldGroup>
-            <Button type="submit" className="bg-[#FFB024] px-8 py-2 text-2xl">
-              {loading ? 'Wait a second' : 'Login'}
-            </Button>
-          </FieldSet>
-        </form>
-      </section>
+      <div className='grid grid-cols-12 *:col-span-6 h-[90vh]'>
+        <section className="flex items-center justify-center">
+          <form
+            className="w-[60%] p-10"
+            onSubmit={handleSubmit}
+          >
+            <FieldSet className="flex h-full flex-col items-center justify-around">
+              <FieldLegend>
+                <p className="text-xl font-semibold">Selamat Datang,</p>
+                <p className='text-sm font-light'>Sistem Dashboard Himasisfo</p>
+              </FieldLegend>
+              <FieldGroup>
+                <Field>
+                  <FieldLabel htmlFor="email" className="text-base font-semibold">
+                    Email
+                  </FieldLabel>
+                  <Input
+                    placeholder='admin123@upnyk.ac.id'
+                    className="bg-[#F2F2F2] border border-[#DFDFDF] h-15 px-4 py-3 rounded-lg placeholder-[#B9B9B9]"
+                    id="email"
+                    type="text"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </Field>
+                <Field>
+                  <FieldLabel htmlFor="password" className="text-base font-semibold">
+                    Password
+                  </FieldLabel>
+                  <Input
+                    placeholder='●●●●●●●●'
+                    className="bg-[#F2F2F2] border border-[#DFDFDF] h-15 px-4 py-3 rounded-lg placeholder-[#B9B9B9]"
+                    id="password"
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </Field>
+              </FieldGroup>
+              <Button type="submit" className="bg-[#3385FF] h-12 px-8 py-2 text-base font-medium w-full">
+                {loading ? '...' : 'Login'}
+              </Button>
+            </FieldSet>
+          </form>
+        </section>
+        <section className='flex flex-col items-center gap-30 justify-center'>
+          <TextSlideshow />
+          <div className='flex flex-col text-center'>
+            <p className='text-md font-semibold'>Himpunan Sistem Informasi</p>
+            <p className='text-sm text-[#1B3C53]/70'>UPN Veteran Yogyakarta</p>
+          </div>
+        </section>
+      </div>
     </>
   );
 }
