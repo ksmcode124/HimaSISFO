57114051be1d4c9beea236a7f81d52388d82ec7b
IvandryPage <ivandry.89@gmail.com>
2026-01-26 22:47:01 +0700

    fix: carousel at 1024x720 button misplaced

--- src/features/kemahasiswaan/components/FolderCard.tsx
+++ src/features/kemahasiswaan/components/FolderCard.tsx
@@ -60,7 +60,7 @@ export function FolderCard({
             ${headerBgClass}
             w-[70%]
             min-h-10 sm:min-h-11 md:min-h-12
-            rounded-t-xl
+            rounded-t-xl overflow-hidden
             translate-y-[1px]
             border-t-2 border-x-2 ${borderClass}
             px-4 md:px-5
--- src/features/kemahasiswaan/sections/PendaftaranVerifikasiDetailSection.tsx
+++ src/features/kemahasiswaan/sections/PendaftaranVerifikasiDetailSection.tsx
@@ -12,9 +12,9 @@ export function PendaftaranVerifikasiDetailSection({ item }: Props) {
   return (
     <section className="grid grid-cols-12 min-h-[50vh] w-full justify-items-center place-items-center relative">
 
-      <div className="relative col-span-12 lg:col-start-4 lg:col-span-6 w-full h-full flex items-center justify-center">
+      <div className="relative col-span-12 lg:col-span-8 lg:col-start-3 xl:col-start-4 xl:col-span-6 w-full h-full flex items-center justify-center">
         <div className="
-          absolute z-30 top-0 right-0 w-22.5 sm:w-36 md:w-52 lg:w-75 aspect-4/3
+          absolute z-30 top-0 right-0 w-22.5 sm:w-36 md:w-52 xl:w-75 aspect-4/3
           -translate-x-[40%] translate-y-[80%]
           md:translate-0
         ">
@@ -27,7 +27,7 @@ export function PendaftaranVerifikasiDetailSection({ item }: Props) {
         </div>
 
         <div className="
-          absolute left-0 bottom-0 rotate-15 w-35 sm:w-52 md:w-80 lg:w-115 aspect-4/3
+          absolute left-0 bottom-0 rotate-15 w-35 sm:w-52 md:w-80 xl:w-115 aspect-4/3
           -translate-x-[5%] translate-y-[8%]
           md:translate-y-[25%] md:-translate-x-[10%]
           lg:translate-y-[35%] lg:-translate-x-[25%]
