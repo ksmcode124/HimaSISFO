f95d3dc110e36442fec8a10e3c4fc374854faf5e
hasbisdqi <muhhasbiassidiqi18@gmail.com>
2026-01-26 16:31:55 +0700

    style: Add background SVG images to Vision section

--- src/features/beranda/sections/vision.tsx
+++ src/features/beranda/sections/vision.tsx
@@ -16,6 +16,10 @@ export default function Vision() {
                 </div>
             </div>
         </div>
+        <img src="/assets/beranda/awan/visitop.svg" className='w-1/4 absolute top-0 right-0 -scale-x-100' alt="" />
+        <img src="/assets/beranda/awan/visitop.svg" className='w-1/4 absolute top-0 left-0' alt="" />
+        <img src="/assets/beranda/awan/visibottom.svg" className='w-1/8 absolute bottom-0 right-0' alt="" />
+        <img src="/assets/beranda/awan/visibottom.svg" className='w-1/8 absolute bottom-0 left-0 -scale-x-100' alt="" />
         <Pita />
     </div>
 
