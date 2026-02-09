dfb7c1663743855e768a1a1cce89064d896a41cd
IvandryPage <ivandry.89@gmail.com>
2026-02-09 11:50:54 +0700

    chore: migration middleware to proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,7 +2,7 @@
 import { NextResponse, type NextRequest } from "next/server";
 import { getSessionCookie } from "better-auth/cookies";
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl;
   const sessionToken = getSessionCookie(request);
 
