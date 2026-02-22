4907fe5e8e9116f1e6a119016bff7f462bfcc312
hasbisdqi <muhhasbiassidiqi18@gmail.com>
2026-02-22 22:03:00 +0700

    feat: update Behind The Web section to use dynamic data for title and author

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -10,6 +10,7 @@ import Awan from "@/features/beranda/components/awan";
 import Pita from "@/features/beranda/components/pita";
 import { ShellLayer } from "@/components/layout/ShellLayer";
 import { SiteFooter } from "@/components/layout";
+import { berandaData } from "@/features/beranda";
 
 
 export default function Page() {
@@ -43,16 +44,18 @@ function Graphics() {
 }
 
 function BehindTheWeb() {
+  const data = berandaData;
+
   return (
     <div className="md:min-h-screen relative grid w-screen aspect-16/10 text-white bg-center items-end mb-30 bg-cover z-0">
 
       <img src="/assets/beranda/code124-bg.png" className="w-full h-full inset-0 absolute object-cover z-15" alt="" />
       {/* CONTENT */}
       <div className="absolute bottom-15 sm:bottom-25 md:bottom-30 lg:bottom-60 w-screen z-20 flex flex-col h-full justify-end items-start px-4 lg:px-16">
-        <p className="text-lg">Behind The Web</p>
+        <p className="text-lg">{data.behind_the_web.title}</p>
         <div className="flex justify-between w-full items-center">
-          <span className="sm:text-7xl text-4xl md:text-[128px]">CODE124</span>
-          <a href="#" className="bg-primary-foreground/5 text-lg lg:text-2xl flex flex-items-center rounded-full px-4 py-2 gap-2 items-center border-white border-2">Selengkapnya <ArrowRight className="size-8" /></a>
+          <span className="sm:text-7xl text-4xl md:text-[128px]">{data.behind_the_web.author}</span>
+          <a href={data.behind_the_web.url} className="bg-primary-foreground/5 text-lg lg:text-2xl flex flex-items-center rounded-full px-4 py-2 gap-2 items-center border-white border-2">Selengkapnya <ArrowRight className="size-8" /></a>
         </div>
       </div>
 
